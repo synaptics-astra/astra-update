cdc105ed5d60ac1ca9d9d4f76ff353ac070b7831
Andrew Duggan <aduggan@synaptics.com>
2025-06-11 14:24:14 -0700

    Add support for multiple SPI images

--- lib/spi_flash_image.hpp
+++ lib/spi_flash_image.hpp
@@ -10,8 +10,8 @@ class SpiFlashImage : public FlashImage
 public:
     SpiFlashImage(std::string imagePath, std::string bootImage, std::string chipName,
             std::string boardName, AstraSecureBootVersion secureBootVersion, AstraMemoryLayout memoryLayout,
-            std::map<std::string, std::string> config) : FlashImage(FLASH_IMAGE_TYPE_SPI, imagePath,
-            bootImage, chipName, boardName, secureBootVersion, memoryLayout, config)
+            std::unique_ptr<std::vector<std::map<std::string, std::string>>> manifestMaps) : FlashImage(FLASH_IMAGE_TYPE_SPI, imagePath,
+            bootImage, chipName, boardName, secureBootVersion, memoryLayout, std::move(manifestMaps))
     {}
     virtual ~SpiFlashImage()
     {}
@@ -19,12 +19,18 @@ public:
     int Load() override;
 
 private:
-    std::string m_readAddress = "0x10000000";
-    std::string m_writeFirstCopyAddress = "0xf0000000";
-    std::string m_writeSecondCopyAddress = "0xf0200000";
-    std::string m_writeLength = "0x200000";
-    std::string m_eraseFirstStartAddress = "0xf0000000";
-    std::string m_eraseFirstEndAddress = "0xf01fffff";
-    std::string m_eraseSecondStartAddress = "0xf0200000";
-    std::string m_eraseSecondEndAddress = "0xf03fffff";
+    struct SpiImageConfig {
+        std::string imageFile;
+        std::string readAddress = "0x10000000";
+        std::string writeFirstCopyAddress = "0xf0000000";
+        std::string writeSecondCopyAddress = "0xf0200000";
+        std::string writeLength = "0x200000";
+        std::string eraseFirstStartAddress = "0xf0000000";
+        std::string eraseFirstLength = "0xf01fffff";
+        std::string eraseSecondStartAddress = "0xf0200000";
+        std::string eraseSecondLength = "0xf03fffff";
+    };
+    std::vector<SpiImageConfig> m_spiImageConfigs;
+
+    void ParseSpiFlashConfig(const std::map<std::string, std::string> &config, std::string imageFile);
 };
