51921957ec0e89b895705553c9e9786b0d1843ed
Andrew Duggan <aduggan@synaptics.com>
2025-09-04 15:33:17 -0700

    Update version to v1.0.5

--- src/astra-boot.cpp
+++ src/astra-boot.cpp
@@ -16,7 +16,7 @@
 #include "flash_image.hpp"
 #include "astra_device.hpp"
 
-const std::string astraBootVersion = "1.0.4";
+const std::string astraBootVersion = "1.0.5";
 
 // Define a struct to hold the two strings
 struct DeviceImageKey {
