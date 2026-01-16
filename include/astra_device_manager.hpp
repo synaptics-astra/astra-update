01b4565468578217a9e5220c5b494265504b1d6e
Andrew Duggan <aduggan@synaptics.com>
2026-01-15 16:41:39 -0800

    Update version to 1.0.5.13

--- include/astra_device_manager.hpp
+++ include/astra_device_manager.hpp
@@ -12,7 +12,7 @@
 #include "flash_image.hpp"
 #include "astra_log.hpp"
 
-#define ASTRA_DEVICE_MANAGER_VERSION "1.0.5.12-detection-sync"
+#define ASTRA_DEVICE_MANAGER_VERSION "1.0.5.13"
 
 enum AstraDeviceManagerStatus {
     ASTRA_DEVICE_MANAGER_STATUS_START,
